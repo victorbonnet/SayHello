2c95dcfacbeb4ac33f53271dc515bbee69cdd2f2
Victor Bonnet <victor.bonnet@gmail.com>
2011-08-21 13:33:21 +0200

    First commit

---
+++ src/com/infine/sayHello/HelloActivity.java
@@ -0,0 +1,35 @@
+package com.infine.sayHello;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HelloActivity extends Activity {
+	private Context context;
+	private TextView textView;
+	private Button button;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello);
+        
+        context = this;
+        textView = (TextView) findViewById(R.id.hello);
+        button = (Button) findViewById(R.id.back);
+        
+        textView.setText(getResources().getString(R.string.hello));
+        
+        button.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				((Activity) context).finish();
+			}
+		});
+    }
+}
---
+++ src/com/infine/sayHello/SayHelloActivity.java
@@ -0,0 +1,42 @@
+package com.infine.sayHello;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SayHelloActivity extends Activity {
+	private final String SAY_HELLO = "sayHello"; 
+	private Context context;
+	private EditText editText;
+	private Button button;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        context = this;
+        editText = (EditText) findViewById(R.id.name);
+        button = (Button) findViewById(R.id.button);
+        
+        button.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				if (editText.getText().toString().equals("")) {
+					Toast.makeText(context, R.string.error, 1000).show();
+				} else {
+					Intent intent = new Intent(context, HelloActivity.class);
+					context.startActivity(intent);
+				}
+				
+			}
+		});
+    }
+}
